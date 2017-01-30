b97cc15b81d01e0228166257994facd9c34f4f2d
x0rz <x0rz@0day.rocks>
2017-01-30 09:55:06 +0100

    Initial version of the script

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# Go to http://dev.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key="xxxxxxxxxxxxxx"
+consumer_secret="xxxxxxxxxxxxx"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token="xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret="xxxxxxxxxxxxxxxxxxxxxxx"
